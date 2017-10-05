941d59381ff21a22c4534556ff9dc8b98bc31ca8
Daniel Chong <danchong625@gmail.com>
2017-10-05 14:27:47 -0700

    [Import] Import nav reducer

--- app/store/reducerConfig.js
+++ app/store/reducerConfig.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import Auth from '../reducers/authReducer';
+import Nav from '../reducers/navReducer';
 
 const TroveReducer = combineReducers({
   Auth,
+  Nav,
 });
 
 export default TroveReducer;
