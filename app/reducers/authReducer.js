89c42a750f974ff771acd7ed41c2114892b02e90
Daniel Chong <danchong625@gmail.com>
2017-10-05 12:34:18 -0700

    [Add] Add user signup reducers

--- app/reducers/authReducer.js
+++ app/reducers/authReducer.js
@@ -1,13 +1,32 @@
 const initialState = {
   loggigIn: false,
-  authenticated: false,
   loggingOut: false,
+  signingIn: false,
+  authenticated: false,
   user: {},
   error: null,
 }
 
 const authReducer = (state=initialState, action) => {
   switch(action.type) {
+    case 'USER_SIGNUP_PENDING': {
+      return Object.assign({}, state, {
+        signingIn: true
+      });
+    }
+    case 'USER_SIGNUP_FULFILLED': {
+      return Object.assign({}, state, {
+        signingIn: false,
+        authenticated: true,
+        user: Object.assign({}, action.payload)
+      });
+    }
+    case 'USER_SIGNUP_REJECTED': {
+      return Object.assign({}, state, {
+        signingIn: false,
+        error: action.payload
+      })
+    }
     case 'USER_LOGIN_PENDING': {
       return Object.assign({}, state, {
         loggigIn: true
