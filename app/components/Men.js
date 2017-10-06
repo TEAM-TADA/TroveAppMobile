e59ee8896d3784a6d192f3b40a58812c148a8d7d
Daniel Chong <danchong625@gmail.com>
2017-10-05 20:17:35 -0700

    [Init] Initialize Men Page

---
+++ app/components/Men.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Men extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>
+          Men's View
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default Men;
