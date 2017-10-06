ce28260f6c1d4369f5583a1f140c08ceb8d32fd3
Daniel Chong <danchong625@gmail.com>
2017-10-06 16:55:12 -0700

    [Rename] Rename to ItemListView

---
+++ app/components/ItemListView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Itemview extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    
+  }
+}
+
+export default Itemview;
