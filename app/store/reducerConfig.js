425b74ebcea52c738adc41260307b7aeb93d0a60
Daniel Chong <danchong625@gmail.com>
2017-10-06 16:56:29 -0700

    [Add] Add item reducer

--- app/store/reducerConfig.js
+++ app/store/reducerConfig.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import Auth from '../reducers/authReducer';
 import Nav from '../reducers/navReducer';
+import Item from '../reducers/itemReducer';
 
 const TroveReducer = combineReducers({
   Auth,
   Nav,
+  Item,
 });
 
 export default TroveReducer;
