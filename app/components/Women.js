52e6831f3b1c8c6b8928636cc17165e638c0a528
Daniel Chong <danchong625@gmail.com>
2017-10-06 14:50:52 -0700

    [Init] Initialize view

---
+++ app/components/Women.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Women extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>
+          Women View
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default Women;
