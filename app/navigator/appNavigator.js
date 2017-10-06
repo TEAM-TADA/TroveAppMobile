e5a09579eeda52735810bf133a01ac08e4f65d59
Daniel Chong <danchong625@gmail.com>
2017-10-05 20:21:44 -0700

    [Update] Implement Navigation and accepts back calls on android

--- app/navigator/appNavigator.js
+++ app/navigator/appNavigator.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
+import { BackHandler } from 'react-native';
 import { connect } from 'react-redux';
-import { addNavigationHelpers, StackNavigator } from 'react-navigation';
+import { addNavigationHelpers, StackNavigator, NavigationActions } from 'react-navigation';
 
-export const AppNavigator = StackNavigator({
-  Main: { screen: MainView },
-  Login: { screen: LoginView },
-});
+import MainView from '../components/Main';
+import LoginView from '../components/Login';
 
-const AppWithNavigationState = ({ dispatch, nav }) => (
-  <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
+export const AppNavigator = StackNavigator(
+  {
+    Main: { 
+      screen: MainView,
+    },
+    Login: { 
+      screen: LoginView,
+    },
+  },
+  {
+    headerMode: 'screen',
+  }
 );
 
-const mapStateToProps = (store) => ({
-  nav: store.Nav,
+class AppWithNavigationState extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+  }
+
+  onBackPress() {
+    const { dispatch, nav } = this.props;
+
+    if (nav.index === 0) {
+      return false;
+    }
+
+    dispatch(NavigationActions.back());
+    return true;
+  }
+
+  render() {
+    const { dispatch, nav } = this.props;
+
+    return <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
+  }
+}
+
+const mapStateToProps = (state) => ({
+  nav: state.Nav,
 });
 
-export default connect(mapStateToProps)(AppWithNavigationState);
+export const App = connect(mapStateToProps)(AppWithNavigationState);
