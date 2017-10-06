ec461550c9964e995a7e0a8b5e7f20d76e71dd9a
Daniel Chong <danchong625@gmail.com>
2017-10-06 15:57:38 -0700

    [Edit] Move navbar to own component

---
+++ app/components/NavBar.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { Button, View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  navBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: "#CDB287"
+  },
+  leftBtn: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    width: 200,
+  },
+  viewBtn: {
+    width: 100,
+  },
+});
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { authenticated, navigation, actions } = this.props;
+
+    if (authenticated) {
+      return (
+        <View style={styles.navBar}>
+          <View style={styles.leftBtn}>
+            <View style={styles.viewBtn}>
+              <Button
+                color="#CDB287"
+                onPress={() => {
+                  navigation.navigate('Men')
+                }}
+                title="Men"
+              />
+            </View>
+            <View style={styles.viewBtn}>
+              <Button
+                color="#CDB287"
+                onPress={() => {
+                  navigation.navigate('Women')
+                }}
+                title="Women"
+              />
+            </View>
+          </View>
+          <Button
+            color="#CDB287"
+            onPress={() => {
+              actions.logout();
+            }}
+            title="Logout"
+          />
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.navBar}>
+          <View style={styles.leftBtn}>
+            <View style={styles.viewBtn}>
+              <Button
+                color="#CDB287"
+                onPress={() => {
+                  navigation.navigate('Men')
+                }}
+                title="Men"
+              />
+            </View>
+            <View style={styles.viewBtn}>
+              <Button
+                color="#CDB287"
+                onPress={() => {
+                  navigation.navigate('Women')
+                }}
+                title="Women"
+              />
+            </View>
+          </View>
+          <Button
+            color="#CDB287"
+            onPress={() => {
+              navigation.navigate('Login');
+            }}
+            title="Log In"
+          />
+        </View>
+      )
+    }
+  }
+}
+
+export default Navbar;
