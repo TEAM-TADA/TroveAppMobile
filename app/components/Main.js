b4e5c57beb32c54f26d933d01db677075464eaa8
Daniel Chong <danchong625@gmail.com>
2017-10-06 15:57:57 -0700

    [Add] Implement carousel view

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { StyleSheet, View, Text, StatusBar, Navigator, Button, ToastAndroid } from 'react-native';
+import Carousel from 'react-native-looped-carousel';
+import { 
+  StyleSheet, View, Text, 
+  StatusBar, Navigator, Button, 
+  ToastAndroid, Dimensions, Image,
+  ScrollView
+} from 'react-native';
 
+import Navbar from './NavBar';
 import * as authActions from '../actions/authActions';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-  },
-  navBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: "#CDB287"
-  },
-  leftBtn: {
-    flexDirection: 'row',
-    flexWrap: 'nowrap',
-    width: 200,
-  },
-  viewBtn: {
-    width: 100,
+    overflow: 'scroll',
+    flexDirection: 'column'
   },
   header: {
     color: '#CDB287',
@@ -29,6 +25,7 @@ const styles = StyleSheet.create({
   }
 });
 
+const { width, height } = Dimensions.get('window');
 
 class MainView extends Component {
   constructor(props) {
@@ -52,75 +49,29 @@ class MainView extends Component {
   render() {
     const { navigation, loggedIn, user, actions } = this.props;
 
-    if (loggedIn) {
-      return (
-        <View style={styles.container}>
-          <View style={styles.navBar}>
-            <View style={styles.leftBtn}>
-              <View style={styles.viewBtn}>
-                <Button
-                  color="#CDB287"
-                  onPress={() => {
-                    navigation.navigate('Men')
-                  }}
-                  title="Men"
-                />
-              </View>
-              <View style={styles.viewBtn}>
-                <Button
-                  color="#CDB287"
-                  onPress={() => {
-                    navigation.navigate('Women')
-                  }}
-                  title="Women"
-                />
-              </View>
-            </View>
-            <Button
-              color="#CDB287"
-              onPress={() => {
-                actions.logout();
-              }}
-              title="Logout"
+    return (
+      <View style={styles.container}>
+        {loggedIn ? <Navbar navigation={navigation} authenticated={true} {...actions} /> : <Navbar navigation={navigation} authenticated={false} {...actions} />}
+        <ScrollView>
+          <Carousel
+            delay={5000}
+            style={{width, height}}
+            autoplay
+          >
+            <Image source={require('../imgs/carousel/2985afe0d520da810af488ba21959b78.jpg')} 
+              resizeMode="contain"
+              resizeMethod="resize" 
             />
+            <Image source={require('../imgs/carousel/977af9d838b7226246678ecc3e7e4bbb.jpg')} resizeMethod="resize" resizeMode="contain" />
+            <Image source={require('../imgs/carousel/ac6d8673dc47abc2722510c565a13524.jpg')} resizeMethod="resize" resizeMode="contain" />
+            <Image source={require('../imgs/carousel/bb5ea3a7615f51e32ceb52d605e708ab.jpg')} resizeMethod="resize" resizeMode="contain" />
+          </Carousel>
+          <View>
+            <Text>Hi</Text>
           </View>
-        </View>
-      )
-    } else {
-      return (
-        <View style={styles.container}>
-          <View style={styles.navBar}>
-            <View style={styles.leftBtn}>
-              <View style={styles.viewBtn}>
-                <Button
-                  color="#CDB287"
-                  onPress={() => {
-                    navigation.navigate('Men')
-                  }}
-                  title="Men"
-                />
-              </View>
-              <View style={styles.viewBtn}>
-                <Button
-                  color="#CDB287"
-                  onPress={() => {
-                    navigation.navigate('Women')
-                  }}
-                  title="Women"
-                />
-              </View>
-            </View>
-            <Button
-              color="#CDB287"
-              onPress={() => {
-                navigation.navigate('Login');
-              }}
-              title="Log In"
-            />
-          </View>
-        </View>
-      )
-    }
+        </ScrollView>
+      </View>
+    )
   }
 }
 
