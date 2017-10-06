b7f764c46f9d34a85a0f59e0492de984f86eca09
Daniel Chong <danchong625@gmail.com>
2017-10-05 22:11:26 -0700

    [Edit] Implement working firebase login/logout (without axios)

--- app/actions/authActions.js
+++ app/actions/authActions.js
@@ -3,37 +3,48 @@ import axios from 'axios';
 
 export const emailSignUp = (name, email, pw) => {
   return function(dispatch) {
+    dispatch({type: 'USER_SIGNUP_PENDING'});
     auth.createUserWithEmailAndPassword(newEmail, pw)
       .then(result => {
-        dispatch({type: 'USER_SIGNUP_PENDING'});
-        axios.post('/api/user', {
-          userName: name,
-          userEmail: email
-        })
-          .then(({ data }) => {
-            dispatch({type: 'USER_SIGNUP_FULFILLED', payload: data});
-          })
-          .catch(err => {
-            dispatch({type: 'USER_SIGNUP_REJECTED', payload: err});
-          });
+        dispatch({type: 'USER_SIGNUP_FULFILLED', payload: result.displayName});
+      //   axios.post('/api/user', {
+      //     userName: name,
+      //     userEmail: email
+      //   })
+      //     .then(({ data }) => {
+      //       dispatch({type: 'USER_SIGNUP_FULFILLED', payload: data});
+      //     })
+      //     .catch(err => {
+      //       dispatch({type: 'USER_SIGNUP_REJECTED', payload: err});
+      //     });
+      // })
+      // .catch(err => {
+      //   dispatch({type: 'USER_SIGNUP_REJECTED', payload: err});
       })
       .catch(err => {
         dispatch({type: 'USER_SIGNUP_REJECTED', payload: err});
-      });
+      })
   };
 };
 
 export const emailLogin = (email, pw) => {
   return function(dispatch) {
+    dispatch({type: 'USER_LOGIN_PENDING'});
     auth.signInWithEmailAndPassword(email, pw)
       .then(result => {
-        axios.get(`/api/user/${email}`)
-          .then(({ data }) => {
-            dispatch({type: 'USER_LOGIN_FULFILLED', payload: data});
-          })
-          .catch(err => {
-            dispatch({type: 'USER_LOGIN_REJECTED', payload: err});
-          });
+        dispatch({type: 'USER_LOGIN_FULFILLED', payload: result.displayName || result.email});
+      //   axios.get(`https://localhost:3000/api/user/${email}`)
+      //     .then(({ data }) => {
+      //       console.log('got data!')
+      //       dispatch({type: 'USER_LOGIN_FULFILLED', payload: data});
+      //     })
+      //     .catch(err => {
+      //       console.log(err);
+      //       dispatch({type: 'USER_LOGIN_REJECTED', payload: err});
+      //     });
+      // })
+      // .catch(err => {
+      //   dispatch({type: 'USER_LOGIN_REJECTED', payload: err});
       })
       .catch(err => {
         dispatch({type: 'USER_LOGIN_REJECTED', payload: err});
