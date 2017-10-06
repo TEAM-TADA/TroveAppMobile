b43e8474c61dfa0e1818becb41094ef3f11d2c98
Daniel Chong <danchong625@gmail.com>
2017-10-06 14:50:13 -0700

    [Update] Style and implement toasts

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,69 +1,123 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { StyleSheet, View, Text, StatusBar, Navigator, Button } from 'react-native';
+import { StyleSheet, View, Text, StatusBar, Navigator, Button, ToastAndroid } from 'react-native';
 
 import * as authActions from '../actions/authActions';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'black',
+  },
+  navBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: "#CDB287"
+  },
+  leftBtn: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    width: 200,
+  },
+  viewBtn: {
+    width: 100,
   },
   header: {
-    color: 'black'
+    color: '#CDB287',
+    textAlign: 'center'
   }
 });
 
+
 class MainView extends Component {
-  static navigationOptions = {
-    title: 'Trove'
+  constructor(props) {
+    super(props);
   }
 
+  static navigationOptions = {
+    headerTitle: 'TROVE',
+    headerTitleStyle: styles.header,
+    headerStyle: {backgroundColor: 'black'},
+  }
+  
+  componentWillUpdate() {
+    const { loggingOut } = this.props;
+
+    if (loggingOut) {
+      ToastAndroid.showWithGravity('Logged Out', ToastAndroid.SHORT, ToastAndroid.BOTTOM);
+    }
+
+  }
   render() {
-    const { navigation, loggedIn, loggingOut, user, actions } = this.props;
+    const { navigation, loggedIn, user, actions } = this.props;
 
     if (loggedIn) {
       return (
         <View style={styles.container}>
-          <Text>Welcome, {user}!</Text>
-          <Button
-            onPress={() => {
-              navigation.navigate('Men')
-            }}
-            title="Men"
-          />
-          <Button
-            onPress={() => {
-              actions.logout();
-            }}
-            title="Logout"
-          />
-        </View>
-      )
-    } else if (loggingOut) {
-      return (
-        <View>
-          <Text>
-            Logging Out..
-          </Text>
+          <View style={styles.navBar}>
+            <View style={styles.leftBtn}>
+              <View style={styles.viewBtn}>
+                <Button
+                  color="#CDB287"
+                  onPress={() => {
+                    navigation.navigate('Men')
+                  }}
+                  title="Men"
+                />
+              </View>
+              <View style={styles.viewBtn}>
+                <Button
+                  color="#CDB287"
+                  onPress={() => {
+                    navigation.navigate('Women')
+                  }}
+                  title="Women"
+                />
+              </View>
+            </View>
+            <Button
+              color="#CDB287"
+              onPress={() => {
+                actions.logout();
+              }}
+              title="Logout"
+            />
+          </View>
         </View>
       )
     } else {
       return (
         <View style={styles.container}>
-          <Text>Hello World! I'm the Mai</Text>
-          <Button
-            onPress={() => {
-              navigation.navigate('Men')
-            }}
-            title="Men"
-          />
-          <Button
-            onPress={() => {
-              navigation.navigate('Login')
-            }}
-            title="Log In"
-          />
+          <View style={styles.navBar}>
+            <View style={styles.leftBtn}>
+              <View style={styles.viewBtn}>
+                <Button
+                  color="#CDB287"
+                  onPress={() => {
+                    navigation.navigate('Men')
+                  }}
+                  title="Men"
+                />
+              </View>
+              <View style={styles.viewBtn}>
+                <Button
+                  color="#CDB287"
+                  onPress={() => {
+                    navigation.navigate('Women')
+                  }}
+                  title="Women"
+                />
+              </View>
+            </View>
+            <Button
+              color="#CDB287"
+              onPress={() => {
+                navigation.navigate('Login');
+              }}
+              title="Log In"
+            />
+          </View>
         </View>
       )
     }
