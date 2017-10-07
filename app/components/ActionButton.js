cfb2d3913398e2ffda07799f83849ed2e89569be
Daniel Chong <danchong625@gmail.com>
2017-10-07 12:20:32 -0700

    [Refactor] Refactor action button to separate component

---
+++ app/components/ActionButton.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ActionButton from 'react-native-action-button';
+import FontAwesome, { Icons } from 'react-native-fontawesome';
+
+class ActionBtn extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { navigation } = this.props;
+
+    return (
+      <ActionButton buttonColor="rgba(205, 178, 135, 1)" position="right">
+        <ActionButton.Item buttonColor="rgba(205, 178, 135, 1)"
+          onPress={() => {
+            navigation.navigate('Cart')
+        }}>
+          <FontAwesome>{Icons.shoppingCart}</FontAwesome>
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor="rgba(205, 178, 135, 1)">
+          <FontAwesome>{Icons.userMd}</FontAwesome>
+        </ActionButton.Item>
+      </ActionButton>
+    )
+  }
+}
+
+export default ActionBtn;
