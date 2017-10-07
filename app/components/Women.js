a8937006c13713c1c4de181e12b74fa749aff0f9
Daniel Chong <danchong625@gmail.com>
2017-10-06 17:52:15 -0700

    [Update] Update womens view to display womens item

--- app/components/Women.js
+++ app/components/Women.js
@@ -1,20 +1,83 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { connect } from 'react-redux';
+
+import FeatureView from './FeatureView';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+  },
+  viewContainer: {
+    alignItems: 'center'
+  },
+  header: {
+    backgroundColor: 'black',
+    height: 50,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  headerText: {
+    color: '#CDB287',
+    textAlign: 'right',
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginLeft: '20%'
+  },
+  headerPage: {
+    color: '#CDB287',
+    textAlign: 'left',
+    marginLeft: 10
+  },
+  headerColumn: {
+    flexDirection: 'column'
+  }
+})
 
 class Women extends Component {
   constructor(props) {
     super(props);
   }
 
+  static navigationOptions = {
+    header: null,
+  }
+
   render() {
+    const { items } = this.props;
+
     return (
-      <View>
-        <Text>
-          Women View
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View>
+            <Text style={styles.headerText}>
+              TROVE
+            </Text>
+          </View>
+          <View>
+            <Text style={styles.headerPage}>
+              Women's Collection
+            </Text>
+          </View>
+        </View>
+        <ScrollView>
+          <View style={styles.viewContainer}>
+            {items.map(item => {
+              return item.sex === 'F' ? <FeatureView item={item} key={item.id + 'm'} /> : null;
+            })}
+          </View>
+        </ScrollView>
       </View>
     )
   }
-}
+};
 
-export default Women;
+const menState = (store) => {
+  return {
+    items: store.Item.items
+  }
+};
+
+export default connect(menState)(Women);
