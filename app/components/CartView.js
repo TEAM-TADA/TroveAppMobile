c8b21e8d8769b3d3173ec59add3d0e5023cfcccf
Daniel Chong <danchong625@gmail.com>
2017-10-07 11:26:39 -0700

    [Init] Initialize cart view

---
+++ app/components/CartView.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+class Cart extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static navigationOptions = {
+    header: null
+  }
+
+  render() {
+
+  }
+}
+
+export default Cart;
