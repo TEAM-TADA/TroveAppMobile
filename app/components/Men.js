bbfa76db86f3497f48ff5749560e81336a4138b9
Daniel Chong <danchong625@gmail.com>
2017-10-06 17:51:58 -0700

    [Update] Update men to display male items

--- app/components/Men.js
+++ app/components/Men.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+import FeatureView from './FeatureView';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+  },
+  viewContainer: {
+    alignItems: 'center'
+  },
+  header: {
     backgroundColor: 'black',
-  }
+    height: 50,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  headerText: {
+    color: '#CDB287',
+    textAlign: 'right',
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginLeft: '23%'
+  },
+  headerPage: {
+    color: '#CDB287',
+    textAlign: 'left',
+    marginLeft: 10
+  },
+  headerColumn: {
+    flexDirection: 'column'
+  },
 })
 
 class Men extends Component {
@@ -18,11 +46,38 @@ class Men extends Component {
   }
 
   render() {
+    const { items } = this.props;
+
     return (
       <View style={styles.container}>
+        <View style={styles.header}>
+          <View>
+            <Text style={styles.headerText}>
+              TROVE
+            </Text>
+          </View>
+          <View>
+            <Text style={styles.headerPage}>
+              Men's Collection
+            </Text>
+          </View>
+        </View>
+        <ScrollView>
+          <View style={styles.viewContainer}>
+            {items.map(item => {
+              return item.sex === 'M' ? <FeatureView item={item} key={item.id + 'm'} /> : null;
+            })}
+          </View>
+        </ScrollView>
       </View>
     )
   }
-}
+};
 
-export default Men;
+const menState = (store) => {
+  return {
+    items: store.Item.items
+  }
+};
+
+export default connect(menState)(Men);
